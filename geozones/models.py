358a2adb4ac2a4559c0ce9905a08e1dda15da721
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2026-04-07 00:28:02 +0200

    Revert "fix: replace unique=True with UniqueConstraint on GeoZone.code"
    This reverts commit 55e4b14d7aacefa73f41400904afcde8ad506736.

--- geozones/models.py
+++ geozones/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class GeoZone(models.Model):
-    code = models.CharField(max_length=20)
+    code = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=255)
     admin_level = models.PositiveSmallIntegerField()
     parent = models.ForeignKey(
@@ -20,12 +20,6 @@ class GeoZone(models.Model):
     class Meta:
         indexes = [
             models.Index(fields=["admin_level"]),
-        ]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["code"],
-                name="geozone_code_unique",
-            ),
         ]
 
     def __str__(self):
